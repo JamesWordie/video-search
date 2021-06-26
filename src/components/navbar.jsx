5c3e83f9c94309e4b84860e89b552588e6c2b06f
James Wordie <james.wordie@yahoo.com>
2021-06-26 22:01:42 +0100

    added styled navbar similar to youtuve

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,18 +3,18 @@ import SearchBar from './search-bar';
 
 const Navbar = ({ onSubmit }) => {
   return (
-    <div className="d-flex justify-content-between w-100" id="navbar ">
-      <div className="d-flex justify-content-start">
-        <i className="fas fa-bars"></i>
+    <div className="d-flex justify-content-between align-items-center w-100" id="navbar ">
+      <div className="d-flex justify-content-start align-items-center w-100">
+        <i className="fas fa-bars mx-3"></i>
         <i className="fab fa-youtube"></i>
-        <p>Youtube</p>
+        <p className="m-0 mx-3 text-white">Youtube</p>
       </div>
       <div className="col-6">
         <SearchBar onSubmit={onSubmit} />
       </div>
-      <div className="d-flex justify-content-between">
-        <i className="fab fa-youtube"></i>
-        <div className="d-flex evenly">
+      <div className="d-flex justify-content-between w-100">
+        <i className="fas fa-microphone mx-3"></i>
+        <div className="d-flex justify-content-evenly w-50">
           <i className="fas fa-video"></i>
           <i className="fas fa-th"></i>
           <i className="fas fa-bell"></i>
