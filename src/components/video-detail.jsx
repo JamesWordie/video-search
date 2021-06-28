c5eb0654530972a8a16718b1eb224156671b20ba
James Wordie <james.wordie@yahoo.com>
2021-06-28 17:13:25 +0100

    commit on new branxh

--- src/components/video-detail.jsx
+++ src/components/video-detail.jsx
@@ -22,11 +22,11 @@ const VideoDetail = ({ video }) => {
         <div className="d-flex justify-content-between">
           <p className="card-text text-muted ">{humanized_time_span(published)}</p>
           <div className="d-flex justify-content-evenly align-items-baseline">
-            <i class="fas fa-thumbs-up mx-2 text-muted"></i><p className="text-muted">{randomLike}</p>
-            <i class="fas fa-thumbs-down mx-2 text-muted"></i><p className="text-muted">{randomDislike}</p>
-            <i class="fas fa-share mx-2 text-muted"></i><p className="text-muted">SHARE</p>
-            <i class="fas fa-bars mx-2 text-muted"></i><p className="text-muted">SAVE</p>
-            <i class="fas fa-ellipsis-h mx-2 text-muted"></i>
+            <i className="fas fa-thumbs-up mx-2 text-muted"></i><p className="text-muted">{randomLike}</p>
+            <i className="fas fa-thumbs-down mx-2 text-muted"></i><p className="text-muted">{randomDislike}</p>
+            <i className="fas fa-share mx-2 text-muted"></i><p className="text-muted">SHARE</p>
+            <i className="fas fa-bars mx-2 text-muted"></i><p className="text-muted">SAVE</p>
+            <i className="fas fa-ellipsis-h mx-2 text-muted"></i>
           </div>
         </div>
       </div>
