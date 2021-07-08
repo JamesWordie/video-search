b73592f4996354d0f466a2e7dbd889a9132572b8
James Wordie <james.wordie@yahoo.com>
2021-07-08 11:49:42 +0100

    trail of transfer back to master

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import './fontawesome.jsx';
 
 ReactDOM.render(
   <App />,
